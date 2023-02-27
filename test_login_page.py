fefad265fd8e0b56ec20e29697bb808663620511
KKariess <110602659+KKariess@users.noreply.github.com>
2023-02-27 13:55:54 +0200

    conftest.py file was created to separate setup

--- test_login_page.py
+++ test_login_page.py
@@ -1,15 +1,7 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 link = "https://www.saucedemo.com/"
-
-
-@pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
-    yield browser
-    browser.quit()
 
 
 @pytest.mark.smoke
